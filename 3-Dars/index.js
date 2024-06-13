51352efd01b407be9e4f8552603188c637ad946a
TurgunovDostonbek <turgunovdostonbek@gmail.com>
2024-06-13 20:43:35 +0500

    Dinamik & Statik Variables and Experssion

---
+++ 3-Dars/index.js
@@ -0,0 +1,23 @@
+// Dinamik & Statik Variables and Experssion.
+
+
+
+// React - bu foydalanuvchi interfeyslarini (UI) ko'rsatish uchun JavaScript kutubxonasi. UI tugmalar, matn va tasvirlar kabi kichik birliklardan qurilgan. React ularni qayta foydalanish mumkin bo'lgan, ichki o'rnatish mumkin bo'lgan komponentlarga birlashtirish imkonini beradi. Veb-saytlardan tortib telefon ilovalarigacha ekrandagi hamma narsani komponentlarga bo'lish mumkin.
+
+
+//Yuqoridagi versiyalar faqat ishlab chiqish uchun mo'ljallangan va ishlab chiqarish uchun mos emas.
+// <script crossorigin src="https://unpkg.com/react@18/umd/react.development.js"></script>
+//<script crossorigin src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"></script>
+
+
+
+// Reactning minimallashtirilgan va optimallashtirilgan ishlab chiqarish versiyalari quyidagi manzilda mavjud:
+//<script crossorigin src="https://unpkg.com/react@18/umd/react.production.min.js"></script>
+//<script crossorigin src="https://unpkg.com/react-dom@18/umd/react-dom.production.min.js"></script>
+
+
+
+//Reaktsiya komponentlari kirish ma'lumotlarini oladigan va ko'rsatilishini qaytaradigan render() usulini qo'llaydi. Ushbu misol JSX deb nomlangan XML-ga o'xshash sintaksisdan foydalanadi. Komponentga kiritilgan kirish ma'lumotlariga this.props orqali render() orqali kirish mumkin.
+
+//JSX ixtiyoriy va React-dan foydalanish shart emas. JSX kompilyatsiya bosqichida ishlab chiqarilgan xom JavaScript kodini ko'rish uchun Babel REPL ni sinab ko'ring.
+// Babel is a JavaScript compiler.
