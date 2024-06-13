df7f85b7763869ba741b26fb82fa4e5a4bea3e2e
TurgunovDostonbek <turgunovdostonbek@gmail.com>
2024-06-13 19:48:14 +0500

    Reactga kirish. React CDN linklari.

---
+++ 1-Dars/index.js
@@ -0,0 +1,21 @@
+// Reactga kirish.
+// React CDN linklari.
+
+
+
+//Yuqoridagi versiyalar faqat ishlab chiqish uchun mo'ljallangan va ishlab chiqarish uchun mos emas.
+// <script crossorigin src="https://unpkg.com/react@18/umd/react.development.js"></script>
+//<script crossorigin src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"></script>
+
+
+
+// Reactning minimallashtirilgan va optimallashtirilgan ishlab chiqarish versiyalari quyidagi manzilda mavjud:
+//<script crossorigin src="https://unpkg.com/react@18/umd/react.production.min.js"></script>
+//<script crossorigin src="https://unpkg.com/react-dom@18/umd/react-dom.production.min.js"></script>
+
+
+
+//Reaktsiya komponentlari kirish ma'lumotlarini oladigan va ko'rsatilishini qaytaradigan render() usulini qo'llaydi. Ushbu misol JSX deb nomlangan XML-ga o'xshash sintaksisdan foydalanadi. Komponentga kiritilgan kirish ma'lumotlariga this.props orqali render() orqali kirish mumkin.
+
+//JSX ixtiyoriy va React-dan foydalanish shart emas. JSX kompilyatsiya bosqichida ishlab chiqarilgan xom JavaScript kodini ko'rish uchun Babel REPL ni sinab ko'ring.
+// Babel is a JavaScript compiler.
