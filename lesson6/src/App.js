99021e030db93d0e4ce16759c65a0f820fb93e10
TurgunovDostonbek <turgunovdostonbek@gmail.com>
2024-06-18 13:13:06 +0500

    Malumotlarni list ko'rinishida chiqarish, Delete items

--- lesson6/src/App.js
+++ lesson6/src/App.js
@@ -1,21 +1,51 @@
 import './App.css';
+import { useState } from "react"
 
 function App() {
-
-  let name = "Dostonbek"
+  let [name, setName] = useState("Dostonbek")
+  const [events, setEvents] = useState([
+    {title: "Dostonbek hello", id: 1 },
+    {title: "Bibisora hello", id: 2 },
+    {title: "Mohinabonu hello", id: 3 }
+  ])
+ 
 
 const handeleClick = () => {
-  name = "BibiSora"
+  setName("Bibisora")
+}
+
+const handeleDelete = (id) => {
+  // 1 - usul
+  ///////////////////////////
+  // const eventDelet = events.filter((event) =>{
+  //   return event.id !== id
+  // })
+  // setEvents(eventDelet)
+  
+// 2 - usul 
+///////////////////////////////
+
+  setEvents((prev) => {
+    return prev.filter((event) => {
+      return event.id !== id
+    })
+  })
 }
 
   return (
     <div className="App">
-      <h1>
-          My name is {name}
-      </h1>
 
+      <h1> My name is {name}</h1>
       <button onClick = {handeleClick}> Change name</button>
-      
+
+      {events.map((item) => {
+        return (
+          <div key={item.id} > 
+            <h1>  {item.title}</h1>
+            <button onClick={() =>  handeleDelete(item.id)}> Delete Me </button>
+          </div>
+        )
+      })}
     </div>
   );
 }
