82dd9c3badaec3d9b1edf78923b1938ae8a89d4a
TurgunovDostonbek <turgunovdostonbek@gmail.com>
2024-07-10 11:31:28 +0500

    JSON Server & useEffect haqida..

---
+++ lesson16/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Trips from './Components/Trips'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <Trips/>
+    </div>
+  )
+}
+
+export default App
---
+++ lesson16/src/Components/Trips.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect, useCallback } from "react"
+
+function Trips() {
+    const [trips, setTrips] = useState([]);
+    
+    console.log(trips);
+
+    useEffect(() => {
+      fetch("http://localhost:3000/trips")
+    .then((res) => res.json())
+    .then((data) => setTrips(data));
+    }, [])
+
+
+
+  return (
+    <div className="trips">
+        {trips.map((item) => {
+          return (
+            <div key={item.id} className="trip-list">
+            <h1>{item.title}</h1>
+            <p>{item.price}</p>
+          </div>
+          )
+        })}
+        <button>Europe</button>
+        <button>America</button>
+    </div>
+  )
+}
+export default Trips
